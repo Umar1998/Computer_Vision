2db4197134ec6153717375b4f00af8154b1e70eb
umar <umara040@gmail.com>
2023-04-07 09:47:55 +0530

    argparse implementation

--- faster-rcnn/src/inference.py
+++ faster-rcnn/src/inference.py
@@ -1,28 +1,34 @@
 import os
 import torchvision
+import argparse
 from utils import object_detection_api
 
-
-# get the pretrained model from torchvision.models
-# Note: pretrained=True will get the pretrained weights for the model.
-# model.eval() to use the model for inference
 
 def load_model():
     """
     load faster rcnn model with resnet 50 backbone 
     from torchvision
 
+    get the pretrained model from torchvision.models
+
     returns model object ready for inference
     """
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
         pretrained=True)
-    model.eval()
+    model.eval()  # model.eval() to use the model for inference
     return model
 
 
 def main():
-    data_path = './faster-rcnn/data'
-    out_path_base = './faster-rcnn/outputs'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', type=str, default='./faster-rcnn/data')
+    parser.add_argument('--out-path-base', type=str,
+                        default='./faster-rcnn/outputs')
+    args = parser.parse_args()
+
+    data_path = args.data_path
+    out_path_base = args.out_path_base
+
     if not os.path.exists(out_path_base):
         os.mkdir(out_path_base)
 
